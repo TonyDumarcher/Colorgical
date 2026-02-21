f717c2eb203083ecf69b03ca0032ee8571dcfdca
Tony <2fflofabre@gmail.com>
2026-02-21 15:56:55 +0100

    made changes to the map, now it's more of a factory game, are you good with that?

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pygame  # python -m pip install pygame-ce
 from player import PlayerClass
-from terrain_gen import TerrainGenClass
+from map import MapClass
 from cycle import DayNightCycleClass
 
 class GameClass:
@@ -16,12 +16,12 @@ class GameClass:
         """self.assets.load_cursor()
         self.assets.load_heart()"""
 
-        self.fps_font = pygame.font.SysFont("Arial", 24, bold=True)
+        self.font = pygame.font.SysFont("Arial", 24, bold=True)
         
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Gardening nightmares")
         self.running = True
-        self.terrain = TerrainGenClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+        self.map = MapClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         #self.player = PlayerClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         self.dayNightCycle = DayNightCycleClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
         
@@ -31,31 +31,44 @@ class GameClass:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 self.running = False
-        
-        self.terrain.move_player(pressed_keys)
-        self.dayNightCycle.update(dividedTime)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4 and self.map.TILE_SIZE < 64: # Scroll up
+                    self.map.TILE_SIZE += 4
+                if event.button == 5 and self.map.TILE_SIZE > 8: # Scroll down
+                    self.map.TILE_SIZE -= 4
+                
+
+
+        self.map.move_player(pressed_keys)
+        #self.dayNightCycle.update(dividedTime)
         
 
     def draw(self):
         self.screen.fill((0, 0, 0))
         
         # Entities
-        self.terrain.draw_terrain(self.screen)
+        self.map.draw_map(self.screen)
         
         # UI 
         self.dayNightCycle.draw(self.screen)
         self.draw_fps()
-        #self.draw_mouse()
+        self.draw_coords()
 
     def draw_fps(self):
         fps_val = int(self.clock.get_fps())
-        fps_surface = self.fps_font.render(f"FPS: {fps_val}", True, (255, 255, 0))
+        fps_surface = self.font.render(f"FPS: {fps_val}", True, (255, 255, 0))
         self.screen.blit(fps_surface, (20, 20))
-
-    def draw_mouse(self):
-        mousePos = pygame.mouse.get_pos()
-        self.screen.blit(self.assets.cursor, mousePos)
         
+    def draw_coords(self):
+        # Draw coordinates
+        x = self.map.x // self.map.TILE_SIZE
+        y =self.map.y // self.map.TILE_SIZE
+        ajoutCoords = len(str(x) + str(y)) - 2 
+        pygame.draw.rect(self.screen,(50,50,50),(50,150,200+ajoutCoords*25,70))
+        pygame.draw.rect(self.screen,(150,150,150),(60,160,180+ajoutCoords*25,50))
+        coordinatesText = self.font.render(f"X:{x} Y:{y}",True,(225,225,225))
+        self.screen.blit(coordinatesText,(70,160))
+
 
 game = GameClass()
 
---
+++ src/map.py
@@ -0,0 +1,99 @@
+import pygame
+import random
+
+class MapClass:
+    def __init__(map, screen_width, screen_height):
+        map.SCREEN_WIDTH = screen_width
+        map.SCREEN_HEIGHT = screen_height
+        map.TILE_SIZE = 16
+        map.PLAYER_SPEED = 10
+
+        map.coreSize = 5
+        map.coreX, map.coreY = 0, 0
+        map.x = (map.coreX * map.TILE_SIZE) - (screen_width // 2) + (map.coreSize * map.TILE_SIZE // 2)
+        map.y = (map.coreY * map.TILE_SIZE) - (screen_height // 2) + (map.coreSize * map.TILE_SIZE // 2)
+        
+        map.ColorPatches = {}
+        map.colorSpacing = 100
+        map.compute_ressources_position(5)
+        map.ModifiedTiles = {}
+        map.SurfaceCache = {}
+
+    def compute_ressources_position(map, amountPerColor):
+        map.Colors = [(255, 60, 60), # Red
+                      (60, 255, 60), # Green
+                      (60, 60, 255)] # Blue
+        
+        for color in map.Colors:
+            for _ in range(amountPerColor):
+                spawnX = random.randint(map.colorSpacing * -1, map.colorSpacing)
+                spawnY = random.randint(map.colorSpacing * -1, map.colorSpacing)
+                if abs(spawnX) < 10 and abs(spawnY) < 10:
+                    spawnX += 20
+                patchLength = random.randint(3, 5)
+                patchSizeHeight = random.randint(3, 5)
+                for x in range(patchLength):
+                    for y in range(patchSizeHeight):
+                        if random.random() > 0.1:
+                            map.ColorPatches[(spawnX + x, spawnY + y)] = color
+
+
+
+
+
+    def move_player(map, keys):
+        nerf = 1
+        keysPressed = [keys[pygame.K_d], keys[pygame.K_q], keys[pygame.K_z], keys[pygame.K_s]]
+        
+        pressedCount = sum(keysPressed)
+        if pressedCount >= 2:
+            nerf = 0.7071
+            
+        if keys[pygame.K_d]:
+            map.x += map.PLAYER_SPEED * nerf
+        if keys[pygame.K_q]:
+            map.x -= map.PLAYER_SPEED * nerf
+        if keys[pygame.K_z]:
+            map.y -= map.PLAYER_SPEED * nerf
+        if keys[pygame.K_s]:
+            map.y += map.PLAYER_SPEED * nerf
+            
+        map.x, map.y = round(map.x), round(map.y)
+
+    def modify_tile(map, mousePos, newTileType):
+        tileX = (map.x + mousePos[0]) // map.TILE_SIZE
+        tileY = (map.y + mousePos[1]) // map.TILE_SIZE
+        if (tileX, tileY) in map.ModifiedTiles:
+            return 
+
+    def draw_map(map, screen):
+        startScreenX = int(map.x // map.TILE_SIZE)
+        startScreenY = int(map.y // map.TILE_SIZE)
+        endScreenX = int((map.x + map.SCREEN_WIDTH) // map.TILE_SIZE) + 2
+        endScreenY = int((map.y + map.SCREEN_HEIGHT) // map.TILE_SIZE) + 3
+
+        for tileY in range(startScreenY, endScreenY):
+            for tileX in range(startScreenX, endScreenX):
+                drawX = tileX * map.TILE_SIZE - map.x
+                drawY = tileY * map.TILE_SIZE - map.y
+                tileColor = (66, 67, 79)
+                outlineColor = (71, 72, 84)
+
+                if (tileX, tileY) in map.ColorPatches:
+                    tileColor = map.ColorPatches[(tileX, tileY)]
+                    outlineColor = (222, 222, 222)
+                pygame.draw.rect(screen, tileColor, (drawX, drawY, map.TILE_SIZE, map.TILE_SIZE))
+                pygame.draw.rect(screen, outlineColor, (drawX, drawY, map.TILE_SIZE, map.TILE_SIZE), 1)
+
+        coreDrawX = (map.coreX * map.TILE_SIZE) - map.x
+        coreDrawY = (map.coreY * map.TILE_SIZE) - map.y
+        corePixelSize = map.coreSize * map.TILE_SIZE
+
+        if (coreDrawX + corePixelSize > 0 and coreDrawX < map.SCREEN_WIDTH and 
+            coreDrawY + corePixelSize > 0 and coreDrawY < map.SCREEN_HEIGHT):
+
+            pygame.draw.rect(screen, (50, 51, 63), (coreDrawX, coreDrawY, corePixelSize, corePixelSize))
+            padding = map.TILE_SIZE // 4
+            pygame.draw.rect(screen, (220, 220, 230), 
+                             (coreDrawX + padding, coreDrawY + padding, 
+                              corePixelSize - (padding * 2), corePixelSize - (padding * 2)))
